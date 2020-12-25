263791dd447080753143dd8fc79e3eb74f25acd7
Miles Johnson <mileswjohnson@gmail.com>
2020-12-25 00:23:22 -0800

    Start on date.

--- src/NEW/schemas/date.ts
+++ src/NEW/schemas/date.ts
@@ -1,5 +1,35 @@
-import { instance } from './instance';
+import { commonCriteria } from '../criteria';
+import createSchema from '../createSchema';
+import { invariant } from '../helpers';
+import { CommonCriterias, Schema } from '../types';
 
-export function date() /* infer */ {
-  return instance().of(Date);
+export interface DateSchema<T> extends Schema<T>, CommonCriterias<DateSchema<T>> {
+  notNullable: () => DateSchema<NonNullable<T>>;
+  nullable: () => DateSchema<T | null>;
+}
+
+function cast(value: unknown): Date {
+  return value instanceof Date ? value : new Date(value as number);
+}
+
+function validateType(value: unknown, path: string) {
+  const time = new Date(value as number);
+
+  invariant(
+    !Number.isNaN(time.getTime()) && time.toString() !== 'Invalid Date',
+    'Must be a string, number, or Date that resolves to a valid Date.',
+    path,
+  );
+
+  return time;
+}
+
+export function date(defaultValue?: Date): DateSchema<Date> {
+  return createSchema({
+    cast,
+    criteria: { ...commonCriteria },
+    defaultValue,
+    type: 'date',
+    validateType,
+  });
 }
